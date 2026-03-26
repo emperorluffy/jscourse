581dcea2d542105d6808e95eb8a126bc6ed2fe2a
Thomas Gakumu <gakumuthomas@gmail.com>
2026-03-26 21:30:42 +0300

    Debugging lesson

--- complete-javascript-course/03-Developer-Skills/starter/script.js
+++ complete-javascript-course/03-Developer-Skills/starter/script.js
@@ -1,5 +1,24 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
-
+/*
 const calcAge = (birthYear) => 2026 - birthYear;
 console.log(calcAge(1996));
+*/
+
+const measureKelvin = function () {
+  const measurement = {
+    type: 'temp',
+    unit: 'celsius',
+    //C) Fix the bug
+    value: Number(prompt('Degree celsius:')),
+  };
+
+  //B Find the bug
+  console.log(measurement);
+  // console.table(measurement);
+  const kelvin = measurement.value + 273;
+  return kelvin;
+};
+
+//A IDENTIFY
+console.log(measureKelvin());
