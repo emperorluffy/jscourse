ea4326f2fc3fffa2adcfd2f9254a88249a577591
Thomas Gakumu <gakumuthomas@gmail.com>
2026-03-27 04:30:40 +0300

    Challenge for the section done

--- complete-javascript-course/03-Developer-Skills/starter/script.js
+++ complete-javascript-course/03-Developer-Skills/starter/script.js
@@ -23,4 +23,34 @@ const measureKelvin = function () {
 //A IDENTIFY
 console.log(measureKelvin());
 
+
+//Challenge
+Given an array of forecasted maximum temperatures, the thermometer displays a string with these temperatures.
+
+Example: [17, 21, 23] will print "... 17ºC in 1 days ... 21ºC in 2 days ... 23ºC in 3 days ..."
+
+Create a function 'printForecast' which takes in an array 'arr' and logs a string like the above to the console.
+
+Use the problem-solving framework: Understand the problem and break it up into sub-problems!
+
+TEST DATA 1: [17, 21, 23]
+TEST DATA 2: [12, 5, -5, 0, 4]
+
+
 */
+const data1 = [17, 21, 23];
+
+const data2 = [12, 5, -5, 0, 4];
+
+// console.log(`..${data[0]}°C...${data[1]}°C..`);
+const printForecast = function (arr) {
+  let str = '... ';
+
+  for (let i = 0; i < arr.length; i++) {
+    str += `${arr[i]}°C in ${i + 1} days ... `;
+  }
+
+  console.log(str);
+};
+
+printForecast([12, 5, -5, 0, 4]);
