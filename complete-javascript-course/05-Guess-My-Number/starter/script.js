9d4e451ff9402607310e5fcbc4281fb20a7a4b25
Thomas Gakumu <gakumuthomas@gmail.com>
2026-03-30 15:51:53 +0300

    Manipulating CSS

--- complete-javascript-course/05-Guess-My-Number/starter/script.js
+++ complete-javascript-course/05-Guess-My-Number/starter/script.js
@@ -9,17 +9,26 @@ document.querySelector('.guess').value = 23;
 */
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-// document.querySelector('.number').textContent = secretNumber;
+document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
+  //When there is no input
+
   if (!guess) {
     document.querySelector('.message').textContent = '⛔️ No number!';
-  } else if (guess === secretNumber) {
+  }
+  // WHEN THE PLAYER WINS THE GAME
+  else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🍾 Correct Number!';
-  } else if (guess > secretNumber) {
+    document.querySelector('body').style.backgroundColor = '#60b347';
+
+    document.querySelector('.number').style.width = '30rem';
+  }
+  //WHEN THE GUESS IS TOO HIGH
+  else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '📈 Too High';
       // score = score -1
@@ -29,7 +38,10 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.message').textContent = '💥 You lost the game!';
       document.querySelector('.score').textContent = 0;
     }
-  } else if (guess < secretNumber) {
+  }
+
+  // WHEN THE GUESS IS TOO LOW
+  else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '📉 Too low';
       // score = score -1
