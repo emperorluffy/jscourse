4587bd859d473be784a986c343765b3c26efc398
Thomas Gakumu <gakumuthomas@gmail.com>
2026-04-06 11:28:44 +0300

    Why I shouldn't use 'var'

--- complete-javascript-course/08-Behind-the-Scenes/starter/script.js
+++ complete-javascript-course/08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,42 @@
 'use strict';
+// HOISTING
+
+//VARIABLES
+console.log(me);
+//console.log(job);
+//console.log(year);
+
+var me = 'Thomas';
+//var in long term will bring bugs
+
+let job = 'enterpreneur';
+const year = '1996';
+
+//FUNCTIONS
+console.log(addDecl(2, 3));
+// console.log(addEpr(2, 3));
+// console.log(addArrow(2, 3));
+
+function addDecl(a, b) {
+  return a + b;
+}
+
+// const addEpr = function (a, b) {
+//   return a + b;
+// };
+
+// const addArrow = (a, b) => a + b;
+//won't work as the function is in temperal dead zone(TDZ)
+
+//Example
+if (!numProducts) deleteShoppingCart();
+
+var numProducts = 10;
+
+function deleteShoppingCart() {
+  console.log('All products deleted');
+}
+
+//The code above will work but it is has a bug of giving wrong ans
+
+//N:B => I declare varibales before using them.
