01059c46220ed73ee00b1a15354e9e1817d42eef
Thomas Gakumu <gakumuthomas@gmail.com>
2026-04-10 15:23:12 +0300

    86' Functions

--- complete-javascript-course/09-Data-Structures-Operators/starter/script.js
+++ complete-javascript-course/09-Data-Structures-Operators/starter/script.js
@@ -986,4 +986,147 @@ const staff = ['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter'];
 
 const staffUnique = [...new Set(staff)];
 console.log(staffUnique);
+
+
+// 1. Add 'name' as a parameter
+const passenger = function (name) {
+  // 2. Use the parameter 'name' instead of the function name
+  const passengerLower = name.toLowerCase();
+
+  const passengerCorrect =
+    passengerLower[0].toUpperCase() + passengerLower.slice(1);
+
+  return passengerCorrect;
+};
+
+// 3. Pass the name as a string using quotes
+console.log(passenger('tHomAs'));
+
+console.log(passenger('jOHn'));
+
+//Booleans
+const plane = 'Airbus A32neo';
+
+console.log(plane.includes('A320'));
+console.log(plane.includes('Boeing'));
+console.log(plane.startsWith('Airb'));
+if (plane.startsWith('Airbus') && plane.endsWith('neo')) {
+  console.log('Part of the New ARirbus family');
+}
+
+//Practice exercise
+const checkBaggage = function (items) {
+  const baggage = items.toLowerCase();
+  if (baggage.includes('knife') || baggage.includes('gun')) {
+    console.log('You are Not allowed on board');
+  } else {
+    console.log('Welcome abroad');
+  }
+};
+checkBaggage('I have a laptop,some food and a pocket knife');
+checkBaggage('Socks and camera');
+checkBaggage('Got some snacks and a gun for protection');
+
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
+
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+  orderDelivery: function ({ starterIndex, mainIndex, time, address }) {
+    console.log(
+      `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`,
+    );
+  },
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(`Here is your delicious pasta with ${ing1},${ing2},${ing3}`);
+  },
+  orderPizza: function (mainIngredient, ...otherIngredients) {
+    console.log(mainIngredient);
+    console.log(otherIngredients);
+  },
+};
+console.log('a+very+nice+string'.split('+'));
+console.log('Thomas Maina'.split(' '));
+
+const [firstName, lastName] = 'Thomas Maina'.split(' ');
+
+const newName = ['Mr.', firstName, lastName.toUpperCase()].join(' ');
+console.log(newName);
+
+const capitalizeName = function (name) {
+  const names = name.split(' ');
+  const namesUpper = [];
+
+  for (const n of names) {
+    //namesUpper.push(n[0].toUpperCase() + n.slice(1));
+    namesUpper.push(n.replace(n[0], n[0].toUpperCase()));
+  }
+  console.log(namesUpper.join(' '));
+};
+
+capitalizeName('thomas maina');
+
+//Padding a str
+const message = 'Go to gate 23!';
+console.log(message.padStart(20, '+').padEnd(30, '+'));
+
+const maskCreditCard = function (number) {
+  const str = number + '';
+  const last = str.slice(-4);
+  return last.padStart(str.length, '*');
+};
+maskCreditCard(4374782478457);
+maskCreditCard(289437448);
+
+console.log(maskCreditCard(4374782478457));
+console.log(maskCreditCard(289437448));
+
+//Repeat
+const message2 = 'Bad weather..All Departures Delayed..';
+console.log(message2.repeat(5));
+
+const planesInLine = function (n) {
+  console.log(`There are ${n} planes in line ${'🛩'.repeat(n)}`);
+};
+planesInLine(5);
 */
+
+document.body.append(document.createElement('textarea'));
+document.body.append(document.createElement('button'));
+
+document.querySelector('button').addEventListener('click', function () {
+  const text = document.querySelector('textarea').value;
+  const rows = text.split('\n');
+
+  for (const row of rows) {
+    const [first, second] = row.toLowerCase().trim().split('_');
+    const output = `${first} ${second.replace(second[0], second[0].toUpperCase)}`;
+    console.log(output);
+  }
+});
+
+// underscoreCase --> underscoreCase
+// firstName
+// someVariable
+// calculateAge
+// delayedDeparture
--- complete-javascript-course/10-Functions/starter/script.js
+++ complete-javascript-course/10-Functions/starter/script.js
@@ -1,1 +1,63 @@
 'use strict';
+/*
+const flight = 'LH234';
+const thomas = {
+  name: 'Thomas Maina',
+  passport: 343948482,
+};
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH4653';
+  passenger.name = 'Mr.' + passenger.name;
+
+  if (passenger.passport === 343948482) {
+    alert('Checked in');
+  } else {
+    alert('Wrong passport!');
+  }
+};
+checkIn(flight, thomas);
+
+// console.log(flight);
+// console.log(thomas);
+// console.log(flight, thomas);
+
+//Is the same as doing
+
+// const flightNum = flight;
+// const passenger = thomas;
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random) * 1000000;
+};
+newPassport(thomas);
+checkIn(flight, thomas);
+*/
+
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+//Higher-order function
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str} `);
+  console.log(`Transformed string ${fn(str)}`);
+
+  console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer('Javascript is the best!', upperFirstWord);
+transformer('Javascript is the best!', oneWord);
+
+const high5 = function () {
+  console.log('✋🏽');
+};
+
+//JS uses callbacks all the time
+document.body.addEventListener('click', high5);
+
+['Thomas', 'Martin', 'Adam'].forEach[high5];
