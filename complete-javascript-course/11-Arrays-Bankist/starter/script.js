0338e665d4ee8b5a565f0e3f33550cb8aef8f835
Thomas Gakumu <gakumuthomas@gmail.com>
2026-04-12 00:37:50 +0300

    Arrays

--- complete-javascript-course/11-Arrays-Bankist/starter/script.js
+++ complete-javascript-course/11-Arrays-Bankist/starter/script.js
@@ -74,3 +74,46 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+
+/*
+let arr = ['a', 'b', 'c', 'd', 'e'];
+
+console.log(arr.slice(2));
+console.log(arr.slice(2, 4));
+
+console.log(arr.slice(-2));
+console.log(arr.slice(1, -2));
+
+console.log(arr.slice());
+console.log([...arr]);
+
+//SPLICE
+
+//console.log(arr.splice(2));
+arr.splice(-1);
+console.log(arr);
+
+//Reverse
+arr = ['a', 'b', 'c', 'd', 'e'];
+const arr2 = ['j', 'i', 'h', 'g', 'f'];
+console.log(arr2.reverse());
+console.log(arr2);
+
+//Concat
+const letters = arr.concat(arr2);
+console.log(letters);
+
+console.log([...arr, ...arr2]);
+
+//Join
+console.log(letters.join('- '));
+
+*/
+const arr = [23, 11, 64];
+console.log(arr[0]);
+console.log(arr.at(0));
+
+//getting last array element
+console.log(arr[arr.length - 1]);
+console.log(arr.slice(-1)[0]);
+console.log(arr.at(-1));
