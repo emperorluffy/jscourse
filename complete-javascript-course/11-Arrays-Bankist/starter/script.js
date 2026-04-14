7e4fe8ffab50a7b3e6b591c6ef20728a542c6ef0
Thomas Gakumu <gakumuthomas@gmail.com>
2026-04-14 11:55:42 +0300

    Call Back Functions

--- complete-javascript-course/11-Arrays-Bankist/starter/script.js
+++ complete-javascript-course/11-Arrays-Bankist/starter/script.js
@@ -197,4 +197,19 @@ currenciesUnique.forEach(function (value, _, map) {
 
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
+const deposits = movements.filter(function (mov) {
+  return mov > 0;
+});
+const depositsFor = [];
+for (const mov of movements) if (mov > 0) depositsFor.push(mov);
+
 console.log(movements);
+console.log(deposits);
+console.log(deposits);
+
+// const withdrawals = movements.filter(function (mov) {
+//   return mov < 0;
+// });
+const withdrawals = movements.filter(mov => mov < 0);
+
+console.log(withdrawals);
