8aed9e0e8c78555853bee0b410536aa432e711d1
Thomas Gakumu <gakumuthomas@gmail.com>
2026-05-18 13:07:49 +0200

    Handler Function

--- complete-javascript-course/13-Advanced-DOM-Bankist/starter/script.js
+++ complete-javascript-course/13-Advanced-DOM-Bankist/starter/script.js
@@ -115,6 +115,28 @@ tabsContainer.addEventListener('click', function (e) {
 });
 
 ////////////////////////////////
+//Menu fade animation
+const nav = document.querySelector('.nav');
+const handleHover = function (e) {
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+
+    const logo = link.closest('.nav').querySelector('img');
+
+    siblings.forEach(el => {
+      if (el !== link) el.style.opacity = this;
+    });
+    logo.style.opacity = this;
+  }
+};
+
+//Pasing "argument" into handler function
+nav.addEventListener('mouseover', handleHover.bind(0.5));
+
+nav.addEventListener('mouseout', handleHover.bind(1));
+
+////////////////////////////////
 ////////////////////////////////
 /////////////////////////////////////////////////////
 /*
