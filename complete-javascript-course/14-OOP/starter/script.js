4d8bcf25290901f482928c1550e63aee54f96d1a
Thomas Gakumu <gakumuthomas@gmail.com>
2026-05-23 15:43:23 +0200

    Constructor functions.

--- complete-javascript-course/14-OOP/starter/script.js
+++ complete-javascript-course/14-OOP/starter/script.js
@@ -1,2 +1,24 @@
 'use strict';
-Opp
+const person = function (firstName, birthYear) {
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+
+  //Never do this
+  //   this.calcAge = function () {
+  //     console.log(2040 - this.birthYear);
+  //   };
+};
+
+const thomas = new person('Thomas', 1996);
+console.log(thomas);
+
+//1.New {} is created
+//2.Function is called, this={}
+//3.{}linked to prototype
+//4.function automatically return{}
+
+const victoria = new person('Victoria', 2005);
+
+const carlos = new person('Noah', 1995);
+
+console.log(victoria, carlos);
