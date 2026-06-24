c2ba0bed8b6470950de807f6fa3ec61a894e3256
Thomas Gakumu <gakumuthomas@gmail.com>
2026-06-24 16:27:59 +0200

    Callback hell

--- complete-javascript-course/16-Asynchronous/starter/script.js
+++ complete-javascript-course/16-Asynchronous/starter/script.js
@@ -1,18 +1,59 @@
 'use strict';
+const renderCountry = function (data) {
+  const html = `
+    <article class="country">
+      <img class="country__img" src="${data.flag}" />
+      <div class="country__data">
+        <h3 class="country__name">${data.name}</h3>
+        <h4 class="country__region">${data.region}</h4>
+        <p class="country__row"><span>👫</span>${(
+          +data.population / 1000000
+        ).toFixed(1)}M people</p>
+        <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+        <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+      </div>
+    </article>
+    `;
 
-const btn = document.querySelector('.btn-country');
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
 const countriesContainer = document.querySelector('.countries');
 
-const request = new XMLHttpRequest();
+const getCountryAndNeighbour = function (country) {
+  //Ajax call country 1
+  const request = new XMLHttpRequest();
 
-// Using an active, stable API endpoint that avoids the 301 redirect
-request.open('GET', 'https://restcountries.de/v3.1/name/kenya');
-request.send();
+  request.open(
+    'GET',
+    `https://countries-api-836d.onrender.com/countries/name/${country}`,
+  );
+  request.send();
 
-request.addEventListener('load', function () {
-  const [data] = JSON.parse(this.responseText);
-  console.log(data);
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
 
-  // Since this is a modern v3.1 structure, name is an object
-  console.log(data.name.common);
-});
+    //Render country 1
+    renderCountry(data);
+
+    //Get neighbour country (2)
+    const [neighbour] = data.borders;
+    if (!neighbour) return;
+
+    //Ajax call country 2
+    const request2 = new XMLHttpRequest();
+
+    request.open(
+      'GET',
+      `https://countries-api-836d.onrender.com/countries/alpha/${neighbour}`,
+    );
+    request.send();
+    request2.addEventListener('load', function () {
+      const data2 = JSON.parse(this.responseText);
+      console.log(data2);
+      renderCountry(data2, 'neighbour');
+    });
+  });
+};
+getCountryAndNeighbour('kenya');
