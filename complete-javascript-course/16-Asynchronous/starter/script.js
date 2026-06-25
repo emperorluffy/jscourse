54cbdc84ae6221dbe10d57cf69ae006e5a422126
Thomas Gakumu <gakumuthomas@gmail.com>
2026-06-25 11:39:50 +0200

    Promises

--- complete-javascript-course/16-Asynchronous/starter/script.js
+++ complete-javascript-course/16-Asynchronous/starter/script.js
@@ -1,4 +1,7 @@
 'use strict';
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
+/*
 const renderCountry = function (data) {
   const html = `
     <article class="country">
@@ -57,3 +60,61 @@ const getCountryAndNeighbour = function (country) {
   });
 };
 getCountryAndNeighbour('kenya');
+*/
+
+// const request = new XMLHttpRequest();
+
+// request.open(
+//   'GET',
+//   `https://countries-api-836d.onrender.com/countries/name/${country}`,
+// );
+// request.send();
+
+// const request = fetch(
+//   'https://countries-api-836d.onrender.com/countries/name/kenya',
+// );
+
+// console.log(request);
+const renderCountry = function (data) {
+  // 1. Safely extract the first language name
+  const lang = Object.values(data.languages)[0];
+
+  // 2. Safely extract the currency name
+  const currency = Object.values(data.currencies)[0].name;
+
+  const html = `
+    <article class="country">
+      <img class="country__img" src="${data.flags.svg}" />
+      <div class="country__data">
+        <h3 class="country__name">${data.name.common || data.name}</h3>
+        <h4 class="country__region">${data.region}</h4>
+        <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)}M people</p>
+        <p class="country__row"><span>🗣️</span>${lang}</p>
+        <p class="country__row"><span>💰</span>${currency}</p>
+      </div>
+    </article>
+  `;
+
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+const getCountryData = function (country) {
+  fetch(`https://countries-api-836d.onrender.com/countries/name/${country}`)
+    .then(function (response) {
+      console.log(response);
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      renderCountry(data[0]);
+    });
+};
+
+// const getCountryData = function (country) {
+//   fetch(`https://countries-api-836d.onrender.com/countries/name/${country}`)
+//     .then(response => response.json())
+//     .then(data => renderCountry(data[0]));
+// };
+
+getCountryData('kenya');
