15affaa83b40b7eee41748a8edba021572a3b93a
Thomas Gakumu <gakumuthomas@gmail.com>
2026-06-27 18:44:59 +0200

    Handling errors and catching errors.

--- complete-javascript-course/16-Asynchronous/starter/script.js
+++ complete-javascript-course/16-Asynchronous/starter/script.js
@@ -1,6 +1,37 @@
 'use strict';
 const btn = document.querySelector('.btn-country');
+
 const countriesContainer = document.querySelector('.countries');
+
+const renderError = function (msg) {
+  countriesContainer.insertAdjacentText('beforeend', msg);
+  // countriesContainer.style.opacity = 1;
+};
+
+const renderCountry = function (data) {
+  // 1. Safely extract the first language name
+  const lang = Object.values(data.languages)[0];
+
+  // 2. Safely extract the currency name
+  const currency = Object.values(data.currencies)[0].name;
+
+  const html = `
+    <article class="country">
+      <img class="country__img" src="${data.flags.svg}" />
+      <div class="country__data">
+        <h3 class="country__name">${data.name.common || data.name}</h3>
+        <h4 class="country__region">${data.region}</h4>
+        <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)}M people</p>
+        <p class="country__row"><span>🗣️</span>${lang}</p>
+        <p class="country__row"><span>💰</span>${currency}</p>
+      </div>
+    </article>
+  `;
+
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  // countriesContainer.style.opacity = 1;
+};
+
 /*
 const renderCountry = function (data) {
   const html = `
@@ -75,46 +106,37 @@ getCountryAndNeighbour('kenya');
 // );
 
 // console.log(request);
-const renderCountry = function (data) {
-  // 1. Safely extract the first language name
-  const lang = Object.values(data.languages)[0];
 
-  // 2. Safely extract the currency name
-  const currency = Object.values(data.currencies)[0].name;
-
-  const html = `
-    <article class="country">
-      <img class="country__img" src="${data.flags.svg}" />
-      <div class="country__data">
-        <h3 class="country__name">${data.name.common || data.name}</h3>
-        <h4 class="country__region">${data.region}</h4>
-        <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)}M people</p>
-        <p class="country__row"><span>🗣️</span>${lang}</p>
-        <p class="country__row"><span>💰</span>${currency}</p>
-      </div>
-    </article>
-  `;
-
-  countriesContainer.insertAdjacentHTML('beforeend', html);
-  countriesContainer.style.opacity = 1;
-};
-
-const getCountryData = function (country) {
+const getCountryData = country => {
   fetch(`https://countries-api-836d.onrender.com/countries/name/${country}`)
-    .then(function (response) {
-      console.log(response);
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data);
+    .then(response => response.json())
+
+    .then(data => {
       renderCountry(data[0]);
+      const neighbour = data[0].borders?.[0];
+      if (!neighbour) return;
+
+      // country 2
+      return fetch(
+        `https://countries-api-836d.onrender.com/countries/name/${neighbour}`,
+      );
+    })
+    .then(response => response.json())
+    .then(data => renderCountry(data[0], 'neighbour'))
+    .catch(err => {
+      console.error(`${err}💥💥🔥`);
+      renderError(`Something went wrong 💥💥💥${err.message}.Try again!`);
+    })
+    .finally(() => {
+      countriesContainer.style.opacity = 1;
     });
 };
-
 // const getCountryData = function (country) {
 //   fetch(`https://countries-api-836d.onrender.com/countries/name/${country}`)
 //     .then(response => response.json())
 //     .then(data => renderCountry(data[0]));
 // };
 
-getCountryData('kenya');
+btn.addEventListener('click', function () {
+  getCountryData('kenya');
+});
