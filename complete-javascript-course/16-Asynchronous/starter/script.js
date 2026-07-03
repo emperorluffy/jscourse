57e8a47cdbefc9db5a3acc63e9bbba3796af5ce5
Thomas Gakumu <gakumuthomas@gmail.com>
2026-07-03 17:25:10 +0200

    Coding challenge 2.Done

--- complete-javascript-course/16-Asynchronous/starter/script.js
+++ complete-javascript-course/16-Asynchronous/starter/script.js
@@ -231,7 +231,7 @@ Promise.reject(new Error('Problem!')).catch(x => console.log(x));
 */
 
 ////////Coding challenge//////////////
-
+/*
 const whereAmI = function () {
   getPosition()
     .then(pos => {
@@ -271,3 +271,49 @@ const getPosition = function () {
 };
 
 btn.addEventListener('click', whereAmI);
+*/
+const wait = function (seconds) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
+const imgContainer = document.querySelector('.images');
+
+const createImage = function (imgPath) {
+  return new Promise(function (resolve, reject) {
+    const img = document.createElement('img');
+    img.src = imgPath;
+
+    img.addEventListener('load', function () {
+      imgContainer.append(img);
+      resolve(img);
+    });
+
+    img.addEventListener('error', function () {
+      reject(new Error('Image not found'));
+    });
+  });
+};
+
+let currentImg;
+
+createImage('img/img-1.jpg')
+  .then(img => {
+    console.log('Image 1 loaded');
+    currentImg = img;
+
+    return wait(2);
+  })
+  .then(() => {
+    currentImg.style.display = 'none';
+    return createImage('img/img-2.jpg');
+  })
+  .then(img => {
+    currentImg = img;
+    console.log('Image 2 loaded');
+    return wait(2);
+  })
+  .then(() => {
+    currentImg.style.display = 'none';
+  })
+  .catch(err => console.log(err));
