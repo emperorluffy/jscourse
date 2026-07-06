c0a876ff35d86f23beaad6588daa65368ac5c264
Thomas Gakumu <gakumuthomas@gmail.com>
2026-07-06 10:34:43 +0200

    First lesson of the day.

---
+++ complete-javascript-course/17-Modern-JS-Modules-Tooling/starter/script.js
@@ -0,0 +1,50 @@
+// import {
+//   addToCart,
+//   shippingCost,
+//   totalPrice as price,
+//   tq,
+// } from './shoppingCart.js';
+
+// addToCart('bread', 5);
+// console.log(price, tq);
+
+console.log('Importing modules');
+// import * as shoppingCart from './shoppingCart.js';
+// shoppingCart.addToCart('bread', 5);
+//console.log(shippingCost);
+// console.log(price);
+
+import add, { cart } from './shoppingCart.js';
+add('pizza', 2);
+add('oranges', 2);
+add('bread', 2);
+
+console.log(cart);
+
+// console.log('Start fetching');
+
+// const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+
+// const data = await res.json();
+
+// console.log(data);
+
+// console.log('Something');
+
+const getLastPost = async function () {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const data = await res.json();
+
+  console.log(data);
+  return { title: data.at(-1).title, text: data.at(-1).body };
+};
+
+const lastPost = getLastPost();
+
+console.log(lastPost);
+
+//Not very clean
+// lastPost.then((last = console.log(last)));
+
+const lastPost2 = await getLastPost();
+console.log(lastPost2);
