e9d95a4974a3ad87eece1ab61426dbad47db77f6
Thomas Gakumu <gakumuthomas@gmail.com>
2026-07-08 12:02:34 +0200

    Modern JS tooling

--- complete-javascript-course/17-Modern-JS-Modules-Tooling/starter/script.js
+++ complete-javascript-course/17-Modern-JS-Modules-Tooling/starter/script.js
@@ -31,20 +31,41 @@ console.log(cart);
 
 // console.log('Something');
 
-const getLastPost = async function () {
-  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
-  const data = await res.json();
+// const getLastPost = async function () {
+//   const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+//   const data = await res.json();
 
-  console.log(data);
-  return { title: data.at(-1).title, text: data.at(-1).body };
-};
+//   console.log(data);
+//   return { title: data.at(-1).title, text: data.at(-1).body };
+// };
 
-const lastPost = getLastPost();
+// const lastPost = getLastPost();
 
-console.log(lastPost);
+// console.log(lastPost);
 
 //Not very clean
 // lastPost.then((last = console.log(last)));
 
-const lastPost2 = await getLastPost();
-console.log(lastPost2);
+// const lastPost2 = await getLastPost();
+// console.log(lastPost2);
+
+// import cloneDeep from './../../../node_modules/lodash-es/cloneDeep.js';
+import cloneDeep from 'lodash-es';
+const state = {
+  cart: [
+    { product: 'bread', quantity: 5 },
+    { product: 'pizza', quantity: 5 },
+  ],
+  user: { loggedIn: true },
+};
+
+const stateClone = Object.assign({}, state);
+const stateDeepClone = cloneDeep(state);
+state.user.loggedIn = false;
+console.log(stateClone);
+
+console.log(stateDeepClone);
+
+if (module.hot) {
+  module.hot.accept();
+}
