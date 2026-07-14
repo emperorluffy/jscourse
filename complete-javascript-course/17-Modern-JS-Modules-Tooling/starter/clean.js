04fd1ebc603be88a9e45d5737779200b9ff30ef1
Thomas Gakumu <gakumuthomas@gmail.com>
2026-07-14 20:27:32 +0200

    Fixed sass bug

--- complete-javascript-course/17-Modern-JS-Modules-Tooling/starter/clean.js
+++ complete-javascript-course/17-Modern-JS-Modules-Tooling/starter/clean.js
@@ -1,4 +1,6 @@
-const budget = [
+'use strict';
+
+const budget = Object.freeze([
   { value: 250, description: 'Sold old TV 📺', user: 'jonas' },
   { value: -45, description: 'Groceries 🥑', user: 'jonas' },
   { value: 3500, description: 'Monthly salary 👩‍💻', user: 'jonas' },
@@ -7,33 +9,50 @@ const budget = [
   { value: -20, description: 'Candy 🍭', user: 'matilda' },
   { value: -125, description: 'Toys 🚂', user: 'matilda' },
   { value: -1800, description: 'New Laptop 💻', user: 'jonas' },
-];
+]);
 
-const spendingLimits = {
+const spendingLimits = Object.freeze({
   jonas: 1500,
   matilda: 100,
-};
+});
+// spendingLimits.jay = 200;
 
-const getLimit = user => spendingLimits?.[user] ?? 0;
+const getLimit = (limits, user) => spendingLimits?.[user] ?? 0;
 
-const addExpense = function (value, description, user = 'jonas') {
-  user = user.toLowerCase();
+const addExpense = function (
+  state,
+  limits,
+  value,
+  description,
+  user = 'jonas',
+) {
+  const cleanUser = user.toLowerCase();
 
   // const limit = spendingLimits[user] ? spendingLimits[user] : 0;
 
-  if (value <= getLimit(user)) {
-    budget.push({ value: -value, description, user });
-  }
+  return value <= getLimit(limits, cleanUser)
+    ? [...state, { value: -value, description, user: cleanUser }]
+    : state;
 };
-addExpense(10, 'Pizza 🍕');
-addExpense(100, 'Going to movies 🍿', 'Matilda');
-addExpense(200, 'Stuff', 'Jay');
+
+const newBudget1 = addExpense(budget, spendingLimits, 1000, 'Pizza 🍕');
+const newBudget2 = addExpense(
+  newBudget1,
+  spendingLimits,
+  100,
+  'Going to movies 🍿',
+  'Matilda',
+);
+const newBudget3 = addExpense(newBudget2, spendingLimits, 200, 'Stuff', 'Jay');
+console.log(newBudget1);
+console.log(newBudget2);
 
 const checkExpenses = function () {
   for (const entry of budget)
     if (entry.value < -getLimit(entry.user)) entry.flag = 'limit';
 };
-checkExpenses();
+checkExpenses(newBudget3, spendingLimits);
+console.log(newBudget3);
 
 const logBigExpenses = function (bigLimit) {
   let output = '';
