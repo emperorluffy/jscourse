c19a1aa7b1ab89f7ae3830e9c0d49c91d08d75c7
Thomas Gakumu <gakumuthomas@gmail.com>
2026-07-15 22:30:34 +0200

    Rendering

--- complete-javascript-course/18-forkify/starter/dist/starter.3464ddca.js
+++ complete-javascript-course/18-forkify/starter/dist/starter.3464ddca.js
@@ -207,11 +207,11 @@
       });
     }
   }
-})({"MD46I":[function(require,module,exports,__globalThis) {
+})({"6gbjq":[function(require,module,exports,__globalThis) {
 var global = arguments[3];
 var HMR_HOST = null;
 var HMR_PORT = null;
-var HMR_SERVER_PORT = 62249;
+var HMR_SERVER_PORT = 1234;
 var HMR_SECURE = false;
 var HMR_ENV_HASH = "d6ea1d42532a7575";
 var HMR_USE_SSE = false;
@@ -715,6 +715,6 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 
 },{}],"j4kuM":[function(require,module,exports,__globalThis) {
 
-},{}]},["MD46I","j4kuM"], "j4kuM", "parcelRequire7e89", {})
+},{}]},["6gbjq","j4kuM"], "j4kuM", "parcelRequire7e89", {})
 
 //# sourceMappingURL=starter.3464ddca.js.map
